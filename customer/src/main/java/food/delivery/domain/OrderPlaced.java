e8662ca3f4d19fcab2afb3085c53327b2d524c29
LSY <89675167+Lsuyeon@users.noreply.github.com>
2023-05-12 05:11:17 +0900

    commit

---
+++ customer/src/main/java/food/delivery/domain/OrderPlaced.java
@@ -0,0 +1,17 @@
+package food.delivery.domain;
+
+import food.delivery.infra.AbstractEvent;
+import java.util.*;
+import lombok.Data;
+
+@Data
+public class OrderPlaced extends AbstractEvent {
+
+    private Long id;
+    private String foodId;
+    private List<String> options;
+    private String address;
+    private String customerId;
+    private String orderId;
+    private String storeId;
+}
